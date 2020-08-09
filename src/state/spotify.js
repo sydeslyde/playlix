d2c522d6d3105e732414ca45daf613b13c1e0f20
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2020-08-09 16:37:18 +0200

    include private and followed playlists

--- src/state/spotify.js
+++ src/state/spotify.js
@@ -4,8 +4,8 @@ const authParams = {
   client_id: 'd35ceaa27c00452aaff6e5ed681a3a09',
   response_type: 'token',
   redirect_uri: window.location.protocol + '//' + window.location.host + '/playlix/#/',
+  scope: 'playlist-read-private'
   //state: 'optional',
-  //scope: 'optional'
 };
 
 export const requestLogin = () => {
